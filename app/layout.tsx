28511cee3b9ba7d5426e54547a97984d7f0a4c40
omgiova <omgiovani4@gmail.com>
2025-09-17 01:32:08 -0300

    Personaliza metadata e package.json com informações da marca

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'Mr Giobot',
+  description: 'Criado por Giovani Amorim',
+  generator: 'MR. GIOBOT',
 }
 
 export default function RootLayout({
